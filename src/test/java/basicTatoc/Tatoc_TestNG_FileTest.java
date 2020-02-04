59915810eb9ea67345367faf61b2bd5ce4d19194
kubra <kubraabbas@qainfotech.com>
2020-02-04 22:36:23 +0530

    third commit

--- src/test/java/basicTatoc/Tatoc_TestNG_FileTest.java
+++ src/test/java/basicTatoc/Tatoc_TestNG_FileTest.java
@@ -50,7 +50,6 @@ public class Tatoc_TestNG_FileTest {
 				driver.switchTo().parentFrame();
 				driver.findElement(By.xpath("//a[text() = 'Repaint Box 2']")).click();
 			}
-			
 		}
 		driver.switchTo().parentFrame();
 		driver.findElement(By.xpath("//a[text() = 'Proceed']")).click();
